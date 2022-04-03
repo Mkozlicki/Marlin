adf7fc26d1ef27fd89d1b0fe892836db4fa8052d
Marcin Kozlicki <marcin.kozlicki@atlascopco.com>
2022-04-03 14:44:18 +0200

    Calibrated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 39, 40, 757 }
 
 /**
  * Default Max Feed Rate (mm/s)
